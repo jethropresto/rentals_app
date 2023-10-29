fdf93ed927a1d2b5d8d08a90b9dea652e0e306fa
Jethro Presto <prestoj@uci.edu>
2023-10-29 10:42:51 -0700

    Successfully added the dashboard pages to the app and tested out responsiveness with the new sidebar
    Dashboard is responsive when sidebar collapses and opens. I also did some management on the overflow in the interest list table.

--- frontend/src/pages/InterestList.jsx
+++ frontend/src/pages/InterestList.jsx
@@ -1,16 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Table from 'react-bootstrap/Table';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
 import axios from 'axios';
 import NewInterestModal from '../components/InterestList/NewInterestModal';
 
 function InterestList() {
 
     const [interestList, setInterestList] = useState([]);
-    const [isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(true);
     const [buttonText, setButtonText] = useState("Show Interest List");
 
     const handleClick = () => {
@@ -30,57 +27,55 @@ function InterestList() {
 
 
     return (
-        <div>
-            <h1>Interest List</h1>
-            
-            <Container>
-                <Row align="center">
-                    <Col>
-                        <Button type="button" onClick={handleClick}>{buttonText}</Button>
-                    </Col>
+        <div className="h-screen sm:px-6 w-full">
+            <div className="h-full flex flex-col">
+                <div className="px-4 md:px-10 py-4 md:py-7">
+                    <div className="flex items-center justify-between">
+                        <p tabindex="0" className="sm:text-lg md:text-xl lg:text-2xl font-bold text-gray-800">Interest List</p>
+                        <div className="py-3 px-4 flex">
+                            <NewInterestModal />
+                        </div>
+                    </div>
+                </div>
 
-                    <Col>
-                        <NewInterestModal></NewInterestModal>
-                    </Col>    
-                </Row>    
-            </Container>
+                <div className="relative overflow-y-auto border">
 
-            <div>
-
-                {isOpen && <Table>
-                    <thead>
-                        <tr>
-                            <th> Name </th>
-                            <th> Phone Number </th>
-                            <th> Event Type </th>
-                            <th> Date </th>
-                            <th> Spaces </th>
-                            <th> Alcohol </th>
-                            <th> Quoted </th>
-                            <th> Tour Date </th>
-                            <th> Status </th>
-                            <th> Last Updated </th>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {interestList.map( (entry) => (
-                            <tr key={entry._id}>
-                                <td>{entry.name}</td>
-                                <td>{entry.phoneNumber}</td>
-                                <td>{entry.eventType}</td>
-                                <td>{entry.date}</td>
-                                <td>{entry.spaces}</td>
-                                <td>{entry.alcohol}</td>
-                                <td>{entry.quoted}</td>
-                                <td>{entry.tourDate}</td>
-                                <td>{entry.status}</td>
-                                <td>{entry.lastUpdated}</td>
+                    {isOpen && <Table className="border border-spacing-4">
+                        <thead className="mt-3 border-b text-sm uppercase bg-gray-100 sticky top-0">
+                            <tr>
+                                <th> Name </th>
+                                <th> Phone Number </th>
+                                <th> Event Type </th>
+                                <th> Date </th>
+                                <th> Spaces </th>
+                                <th> Alcohol </th>
+                                <th> Quoted </th>
+                                <th> Tour Date </th>
+                                <th> Status </th>
+                                <th> Last Updated </th>
                             </tr>
-                        ))}
-                    </tbody>
-                </Table>}
+                        </thead>
+
+                        <tbody>
+                            {interestList.map( (entry) => (
+                                <tr key={entry._id}>
+                                    <td>{entry.name}</td>
+                                    <td>{entry.phoneNumber}</td>
+                                    <td>{entry.eventType}</td>
+                                    <td>{entry.date}</td>
+                                    <td>{entry.spaces}</td>
+                                    <td>{entry.alcohol}</td>
+                                    <td>{entry.quoted}</td>
+                                    <td>{entry.tourDate}</td>
+                                    <td>{entry.status}</td>
+                                    <td>{entry.lastUpdated}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>}
+                </div>
             </div>
+        
         </div>
     )
 }
